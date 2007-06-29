76153e7a6bce27d4de02bca0ce2dce003b034cde
knipknap <knipknap@4c10cf09-d433-0410-9a0a-09c53010615a>
2007-06-29 20:07:12 +0000

    * Added README and legal stuff. * Dumped obsolete test. -Samuel
    git-svn-id: https://exscript.googlecode.com/svn/trunk@10 4c10cf09-d433-0410-9a0a-09c53010615a

--- exscript.py
+++ exscript.py
@@ -3,6 +3,20 @@
 ## Date:        2007-06-04
 ## Description: Use the EScript interpreter with a multi threaded configuration
 ##              engine to execute commands on a list of hosts.
+# Copyright (C) 2007 Samuel Abels, http://debain.org
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 import sys, time, os, re, signal
 sys.path.insert(0, 'lib')
 import Exscript
