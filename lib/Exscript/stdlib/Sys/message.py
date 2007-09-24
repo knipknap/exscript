6b4a9aeb121fcf4eec9114499a8fe263ebfc50e1
knipknap <knipknap@4c10cf09-d433-0410-9a0a-09c53010615a>
2007-09-24 07:00:51 +0000

    * The sys.message() function now appends a newline character. -Samuel
    git-svn-id: https://exscript.googlecode.com/svn/trunk@68 4c10cf09-d433-0410-9a0a-09c53010615a

--- lib/Exscript/stdlib/Sys/message.py
+++ lib/Exscript/stdlib/Sys/message.py
@@ -3,6 +3,6 @@ import sys
 signature = [('self', 'string')]
 
 def execute(scope, string):
-    sys.stdout.write(string[0])
+    sys.stdout.write(string[0] + '\n')
     sys.stdout.flush()
     return 1
