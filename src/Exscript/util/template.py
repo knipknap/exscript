80ac7e6ca13eaeffe31ec801200ce818c3dfa67e
Samuel Abels <sab@nmc-m.dtag.de>
2009-12-04 13:24:46 +0100

    add API docs for util.template.

--- src/Exscript/util/template.py
+++ src/Exscript/util/template.py
@@ -33,24 +33,125 @@ def _run(conn, template, parser_kwargs, **kwargs):
     compiled = _compile(template, parser_kwargs, **kwargs)
     return compiled.execute()
 
-def test(template, **kwargs):
+def test(string, **kwargs):
+    """
+    Compiles the given template, and raises an exception if that
+    failed. Does nothing otherwise.
+
+    @type  string: string
+    @param string: The template to compile.
+    @type  kwargs: dict
+    @param kwargs: Variables to define in the template.
+    """
     kwargs.update(_builtin_vars())
-    return _compile(template, {}, **kwargs)
+    _compile(string, {}, **kwargs)
+
+def test_file(filename, **kwargs):
+    """
+    Convenience wrapper around test() that reads the template from a file
+    instead.
+
+    @type  filename: string
+    @param filename: The name of the template file.
+    @type  kwargs: dict
+    @param kwargs: Variables to define in the template.
+    """
+    kwargs.update(_builtin_vars())
+    _compile(string, {}, **kwargs)
 
 def eval(conn, string, strip_command = True, **kwargs):
+    """
+    Compiles the given template and executes it on the given
+    connection.
+    Raises an exception if the compilation fails.
+
+    if strip_command is True, the first line of each response that is
+    received after any command sent by the template is stripped. For
+    example, consider the following template:
+
+        ls -1{extract /(\S+)/ as filenames}
+        {loop filenames as filename}
+            touch $filename
+        {end}
+
+    If strip_command is False, the response, (and hence, the `filenames'
+    variable) contains the following:
+
+        ls -1
+        myfile
+        myfile2
+        [...]
+
+    By setting strip_command to True, the first line is ommitted.
+
+    @type  conn: Connection
+    @param conn: The connection on which to run the template.
+    @type  string: string
+    @param string: The template to compile.
+    @type  strip_command: bool
+    @param strip_command: Whether to strip the command echo from the response.
+    @type  kwargs: dict
+    @param kwargs: Variables to define in the template.
+    @rtype:  dict
+    @return: The variables that are defined after execution of the script.
+    """
     kwargs.update(_builtin_vars(conn))
     return _run(conn, string, {'strip_command': strip_command}, **kwargs)
 
-def paste(conn, string, **kwargs):
-    kwargs.update(_builtin_vars(conn))
-    return _run(conn, string, {'no_prompt': True}, **kwargs)
-
 def eval_file(conn, filename, strip_command = True, **kwargs):
+    """
+    Convenience wrapper around eval() that reads the template from a file
+    instead.
+
+    @type  conn: Connection
+    @param conn: The connection on which to run the template.
+    @type  filename: string
+    @param filename: The name of the template file.
+    @type  strip_command: bool
+    @param strip_command: Whether to strip the command echo from the response.
+    @type  kwargs: dict
+    @param kwargs: Variables to define in the template.
+    """
     kwargs.update(_builtin_vars(conn, filename))
     template = open(filename, 'r').read()
     return _run(conn, template, {'strip_command': strip_command}, **kwargs)
 
+def paste(conn, string, **kwargs):
+    """
+    Compiles the given template and executes it on the given
+    connection. This function differs from eval() such that it does not
+    wait for a prompt after sending each command to the connected host.
+    That means that the script can no longer read the response of the
+    host, making commands such as `extract' or `set_prompt' useless.
+
+    The function raises an exception if the compilation fails, or if
+    the template contains a command that requires a response from the
+    host.
+
+    @type  conn: Connection
+    @param conn: The connection on which to run the template.
+    @type  string: string
+    @param string: The template to compile.
+    @type  kwargs: dict
+    @param kwargs: Variables to define in the template.
+    @rtype:  dict
+    @return: The variables that are defined after execution of the script.
+    """
+    kwargs.update(_builtin_vars(conn))
+    return _run(conn, string, {'no_prompt': True}, **kwargs)
+
 def paste_file(conn, filename, **kwargs):
+    """
+    Convenience wrapper around paste() that reads the template from a file
+    instead.
+
+    @type  conn: Connection
+    @param conn: The connection on which to run the template.
+    @type  filename: string
+    @param filename: The name of the template file.
+    @type  kwargs: dict
+    @param kwargs: Variables to define in the template.
+    """
     kwargs.update(_builtin_vars(conn, filename))
     template = open(filename, 'r').read()
     return _run(conn, template, {'no_prompt': True}, **kwargs)
