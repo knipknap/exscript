a107889cd376ad9f633b3811762ce7466d00d73c
Samuel Abels <sab@nmc-m.dtag.de>
2009-12-04 12:03:56 +0100

    util: Add documentation for decorator functions.

--- src/Exscript/util/decorators.py
+++ src/Exscript/util/decorators.py
@@ -17,20 +17,83 @@ from Exscript             import stdlib
 from Exscript.Interpreter import Parser
 from Exscript.FooLib      import Interact
 
-def os_function_mapper(conn, map, *data, **kwargs):
+def os_function_mapper(conn, map, *args, **kwargs):
+    """
+    When called with an open connection, this function uses the
+    conn.guess_os() function to determine the operating system
+    that is running on the connected host.
+    It then uses the given map to look up a function name that corresponds
+    to the operating system, and calls it. Example:
+
+        def ios(conn):
+            pass # Do something.
+
+        def junos(conn):
+            pass # Do something else.
+
+        def shell(conn):
+            pass # Do something else.
+
+        Exscript.util.start.quickrun('myhost', os_function_mapper)
+
+    An exception is raised if a matching function is not found in the map.
+
+    @type  conn: Connection
+    @param conn: The open connection.
+    @type  map: dict
+    @param map: A dictionary mapping operating system name to a function.
+    @type  args: list
+    @param args: Passed on to the called function.
+    @type  kwargs: dict
+    @param kwargs: Passed on to the called function.
+    @rtype:  object
+    @return: The return value of the called function.
+    """
     os   = conn.guess_os()
     func = map.get(os)
     if func is None:
         raise Exception('No handler for %s found.' % os)
-    return func(conn, *data, **kwargs)
+    return func(conn, *args, **kwargs)
 
 def connect(function):
+    """
+    Wraps the given function such that the connection is opened before
+    calling it. Example:
+
+        def my_func(conn):
+            pass # Do something.
+        Exscript.util.start.quickrun('myhost', connect(my_func))
+
+    @type  function: function
+    @param function: The function that's ought to be wrapped.
+    @rtype:  function
+    @return: The wrapped function.
+    """
     def decorated(conn, *args, **kwargs):
         conn.open()
         function(conn, *args, **kwargs)
     return decorated
 
 def autologin(function, wait = True):
+    """
+    Wraps the given function such that
+
+        - the connection is opened, and
+        - the user is logged in
+
+    before calling calling it. Example:
+
+        def my_func(conn):
+            pass # Do something.
+        Exscript.util.start.quickrun('myhost', autologin(my_func))
+
+    @type  function: function
+    @param function: The function that's ought to be wrapped.
+    @type  wait: bool
+    @param wait: Whether to wait for a prompt after logging in.
+    @rtype:  function
+    @return: The wrapped function.
+    """
     def decorated(conn, *args, **kwargs):
         conn.authenticate(wait = wait)
         function(conn, *args, **kwargs)
