9e18f97cbc3c8fb6743592124d3316ed5a759350
Samuel Abels <knipknap@gmail.com>
2009-12-08 14:19:43 +0100

    Update legalese and contact address, and remove useless junk from the changelog.

--- src/Exscript/util/url.py
+++ src/Exscript/util/url.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2007-2009 Samuel Abels, http://debain.org
+# Copyright (C) 2007-2009 Samuel Abels.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2, as
