fc65eb1703c5565f8433cf67da32e7d8547bea2a
Samuel Abels <knipknap@gmail.com>
2009-12-13 17:33:58 +0100

    Add a demos/ folder with two demos.

---
+++ demos/report.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from Exscript             import Queue, Logger
+from Exscript.util.file   import get_hosts_from_file, get_accounts_from_file
+from Exscript.util.report import status, summarize
+
+def do_something(conn):
+    conn.open()
+    conn.authenticate()
+    conn.execute('show ip int brie')
+
+# Read input data.
+accounts = get_accounts_from_file('accounts.cfg')
+hosts    = get_hosts_from_file('hostlist.txt')
+
+# Run do_something on each of the hosts. The given accounts are used
+# round-robin. "verbose = 0" instructs the queue to not generate any
+# output on stdout. Using "logdir = ..." is equivalent to the following:
+#   logger = FileLogger(queue, 'my/logdir')
+# It instructs the queue to automatically log everything to the filesystem;
+# one file is created per host.
+queue  = Queue(verbose = 0, max_threads = 5, logdir = 'my/logdir/')
+logger = Logger(queue)          # Logs everything to memory.
+queue.add_account(accounts)     # Adds one or more accounts.
+queue.run(hosts, do_something)  # Asynchronously enqueues all hosts.
+queue.shutdown()                # Waits until all hosts are completed.
+
+# Print a short report.
+print status(logger)
+print summarize(logger)
---
+++ demos/simple.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from Exscript.util.file      import get_hosts_from_file
+from Exscript.util.match     import any_match
+from Exscript.util.template  import eval_file
+from Exscript.util.start     import quickrun
+from Exscript.util.decorator import autologin
+
+def do_something(conn):
+    conn.execute('show ip int brie')
+    ge_interfaces = any_match(conn, r'(Gigabit\S+)')
+    eval_file(conn, 'mytemplate.exscript', interfaces = ge_interfaces)
+
+# Read a list of hostnames from a file.
+hosts = get_hosts_from_file('hostlist.txt')
+
+# Open a connection (Telnet, by default) to each of the hosts, and run
+# do_something(). To open the connection via SSH, you may prefix the
+# hostname by the protocol, e.g.: 'ssh://hostname', 'telnet://hostname',
+# etc.
+# The "max_threads" keyword indicates the maximum number of concurrent
+# connections.
+quickrun(hosts, autologin(do_something), max_threads = 5)
