f3d947d7a55aaa38b592bcad32b1748ba9fbd742
Samuel Abels <knipknap@gmail.com>
2009-12-23 15:44:53 +0100

    Add util.mail.

---
+++ src/Exscript/util/mail.py
@@ -0,0 +1,336 @@
+#!/usr/bin/python
+import os, time, re, socket
+
+###########################################################
+# Helpers. (non-public)
+###########################################################
+_varname_re = re.compile(r'[a-z][\w_]*',     re.I)
+_string_re  = re.compile(r'(\\?){([\w_]+)}', re.I)
+
+class _TemplateParser(object):
+    """
+    This exists for backward compatibility; Python 2.3 does not come
+    with a similar way for string substitution yet.
+    """
+    def __init__(self):
+        self.tmpl_vars = None
+
+    # Tokens that include variables in a string may use this callback to
+    # substitute the variable against its value.
+    def _variable_sub_cb(self, match):
+        escape  = match.group(1)
+        varname = match.group(2)
+        if escape == '\\':
+            return '$' + varname
+        if not _varname_re.match(varname):
+            raise Exception('%s is not a variable name' % varname)
+        value = self.tmpl_vars.get(varname)
+        if value is None:
+            raise Exception('Undefined value for %s' % varname)
+        elif hasattr(value, '__iter__'):
+            value = '\n'.join([str(v) for v in value])
+        return str(value)
+
+    def parse(self, template, **kwargs):
+        self.tmpl_vars = kwargs
+        output         = ''
+        for line in template.split('\n'):
+            if line.endswith(' '):
+                output += line
+            else:
+                output += line + '\n'
+        return _string_re.sub(self._variable_sub_cb, output)
+
+def _render_template(string, **vars):
+    default = {'date': time.strftime('%Y-%m-%d'),
+               'user': os.environ.get('USER')}
+    default.update(vars)
+    parser = _TemplateParser()
+    return parser.parse(string, **default)
+
+###########################################################
+# Public.
+###########################################################
+class Mail(object):
+    """
+    Represents an email.
+    """
+
+    def __init__(self,
+                 sender  = None,
+                 to      = '',
+                 cc      = '',
+                 bcc     = '',
+                 subject = '',
+                 body    = ''):
+        """
+        Creates a new email with the given values.
+        If the given sender is None, one will be automatically chosen
+        from the USER environment variable.
+
+        @type  sender: string
+        @param sender: The email address of the sender.
+        @type  to: string|list(string)
+        @param to: A list of email addresses, passed to set_to().
+        @type  cc: string|list(string)
+        @param cc: A list of email addresses, passed to set_cc().
+        @type  bcc: string|list(string)
+        @param bcc: A list of email addresses, passed to set_bcc().
+        @type  subject: string
+        @param subject: A subject line, passed to set_subject().
+        @type  body: string
+        @param body: The email body, passed to set_body().
+        """
+        if not sender:
+            domain = socket.getfqdn('localhost')
+            sender = os.environ.get('USER') + '@' + domain
+        self.set_sender(sender)
+        self.set_to(to)
+        self.set_cc(cc)
+        self.set_bcc(bcc)
+        self.set_subject(subject)
+        self.set_body(body)
+
+    def set_from_template_string(self, string):
+        """
+        Reads the given template (SMTP formatted) and sets all fields
+        accordingly.
+
+        @type  string: string
+        @param string: The template.
+        """
+        in_header = True
+        body      = ''
+        for line in string.split('\n'):
+            if not in_header:
+                body += line + '\n'
+                continue
+            if not self._is_header_line(line):
+                body += line + '\n'
+                in_header = False
+                continue
+            key, value = self._get_var_from_header_line(line)
+            if key == 'from':
+                self.set_sender(value)
+            elif key == 'to':
+                self.set_to(value)
+            elif key == 'cc':
+                self.set_cc(value)
+            elif key == 'bcc':
+                self.set_bcc(value)
+            elif key == 'subject':
+                self.set_subject(value)
+            else:
+                raise Exception('Invalid header field "%s"' % key)
+        self.set_body(body)
+
+    def _is_header_line(self, line):
+        return re.match(r'^\w+: .+$', line) is not None
+
+    def _get_var_from_header_line(self, line):
+        match = re.match(r'^(\w+): (.+)$', line)
+        return match.group(1).strip().lower(), match.group(2).strip()
+
+    def _cleanup_mail_addresses(self, receipients):
+        if not isinstance(receipients, str):
+            receipients = ','.join(receipients)
+        rcpt = re.split(r'\s*[,;]\s*', receipients.lower())
+        return [r for r in rcpt if r.strip()]
+
+    def set_sender(self, sender):
+        """
+        Defines the value of the "From:" field.
+
+        @type  sender: string
+        @param sender: The email address of the sender.
+        """
+        self.sender = sender
+
+    def get_sender(self):
+        """
+        Returns the value of the "From:" field.
+
+        @rtype:  string
+        @return: The email address of the sender.
+        """
+        return self.sender
+
+    def set_to(self, to):
+        """
+        Replaces the current list of receipients in the 'to' field by
+        the given value. The value may be one of the following:
+
+          - A list of strings (email addresses).
+          - A comma separated string containing one or more email addresses.
+
+        @type  to: string|list(string)
+        @param to: The email addresses for the 'to' field.
+        """
+        self.to = self._cleanup_mail_addresses(to)
+
+    def add_to(self, to):
+        """
+        Adds the given list of receipients to the 'to' field.
+        Accepts the same argument types as set_to().
+
+        @type  to: string|list(string)
+        @param to: The list of email addresses.
+        """
+        self.to += self._cleanup_mail_addresses(to)
+
+    def get_to(self):
+        """
+        Returns the value of the "to" field.
+
+        @rtype:  list(string)
+        @return: The email addresses in the 'to' field.
+        """
+        return self.to
+
+    def set_cc(self, cc):
+        """
+        Like set_to(), but for the 'cc' field.
+
+        @type  cc: string|list(string)
+        @param cc: The email addresses for the 'cc' field.
+        """
+        self.cc = self._cleanup_mail_addresses(cc)
+
+    def add_cc(self, cc):
+        """
+        Like add_to(), but for the 'cc' field.
+
+        @type  cc: string|list(string)
+        @param cc: The list of email addresses.
+        """
+        self.cc += self._cleanup_mail_addresses(cc)
+
+    def get_cc(self):
+        """
+        Returns the value of the "cc" field.
+
+        @rtype:  list(string)
+        @return: The email addresses in the 'cc' field.
+        """
+        return self.cc
+
+    def set_bcc(self, bcc):
+        """
+        Like set_to(), but for the 'bcc' field.
+
+        @type  bcc: string|list(string)
+        @param bcc: The email addresses for the 'bcc' field.
+        """
+        self.bcc = self._cleanup_mail_addresses(bcc)
+
+    def add_bcc(self, bcc):
+        """
+        Like add_to(), but for the 'bcc' field.
+
+        @type  bcc: string|list(string)
+        @param bcc: The list of email addresses.
+        """
+        self.bcc += self._cleanup_mail_addresses(bcc)
+
+    def get_bcc(self):
+        """
+        Returns the value of the "bcc" field.
+
+        @rtype:  list(string)
+        @return: The email addresses in the 'bcc' field.
+        """
+        return self.bcc
+
+    def get_receipients(self):
+        """
+        Returns a list of all receipients (to, cc, and bcc).
+
+        @rtype:  list(string)
+        @return: The email addresses of all receipients.
+        """
+        return self.get_to() + self.get_cc() + self.get_bcc()
+
+    def set_subject(self, subject):
+        """
+        Defines the subject line.
+
+        @type  subject: string
+        @param subject: The new subject line.
+        """
+        self.subject = subject
+
+    def get_subject(self):
+        """
+        Returns the subject line.
+
+        @rtype:  string
+        @return: The subject line.
+        """
+        return self.subject
+
+    def set_body(self, body):
+        """
+        Defines the body of the mail.
+
+        @type  body: string
+        @param body: The new email body.
+        """
+        self.body = body.strip() + '\n'
+
+    def get_body(self):
+        """
+        Returns the body of the mail.
+
+        @rtype:  string
+        @return: The body of the mail.
+        """
+        return self.body
+
+    def get_smtp_header(self):
+        """
+        Returns the SMTP formatted header of the line.
+
+        @rtype:  string
+        @return: The SMTP header.
+        """
+        header  = "From: %s\r\n"    % self.get_sender()
+        header += "To: %s\r\n"      % ',\r\n '.join(self.get_to())
+        header += "Cc: %s\r\n"      % ',\r\n '.join(self.get_cc())
+        header += "Bcc: %s\r\n"     % ',\r\n '.join(self.get_bcc())
+        header += "Subject: %s\r\n" % self.get_subject()
+        return header
+
+    def get_smtp_mail(self):
+        """
+        Returns the SMTP formatted email, as it may be passed to sendmail.
+
+        @rtype:  string
+        @return: The SMTP formatted mail.
+        """
+        header = self.get_smtp_header()
+        body   = self.get_body().replace('\n', '\r\n')
+        return header + '\r\n' + body
+
+def from_template_string(string, **vars):
+    """
+    Reads the given SMTP formatted template, and creates a new Mail object
+    using the information.
+
+    @type  string: string
+    @param string: The SMTP formatted template.
+    """
+    tmpl = _render_template(string, **vars)
+    mail = Mail()
+    mail.set_from_template_string(tmpl)
+    return mail
+
+def from_template(filename, **vars):
+    """
+    Like from_template_string(), but reads the template from the file with
+    the given name instead.
+
+    @type  filename: string
+    @param filename: The name of the template file.
+    """
+    tmpl = open(filename).read()
+    return from_template_string(tmpl, **vars)
---
+++ tests/Exscript/util/mailTest.py
@@ -0,0 +1,165 @@
+import sys, unittest, re, os.path, tempfile
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..', '..', 'src'))
+
+import Exscript.util.mail
+from Exscript.util.mail import Mail
+
+vars     = dict(testvar1 = 'blah', testvar2 = 'foo', testvar3 = 'bar')
+template = '''
+To: user, user2@localhost
+Cc: user3
+Bcc: user4
+Subject: Blah blah {testvar1}
+
+Test body: {testvar2}
+{testvar3}
+'''.strip()
+
+smtp = '''
+From: test
+To: user,
+ user2@localhost
+Cc: user3
+Bcc: user4
+Subject: Blah blah {testvar1}
+
+Test body: {testvar2}
+{testvar3}
+'''.strip().replace('\n', '\r\n')
+
+class MailTest(unittest.TestCase):
+    CORRELATE = Mail
+
+    def setUp(self):
+        self.mail = Mail(sender = 'test')
+
+    def testConstructor(self):
+        mail = Mail()
+        self.failIfEqual(mail.get_sender(), None)
+        self.failIfEqual(mail.get_sender(), '')
+        user = os.environ.get('USER')
+        self.assert_(mail.get_sender().startswith(user + '@'))
+
+    def testSetFromTemplateString(self):
+        self.mail.set_from_template_string(template)
+        tmpl = self.mail.get_smtp_mail().strip()
+        self.assertEqual(tmpl, smtp)
+        head = self.mail.get_smtp_header().strip()
+        self.assert_(tmpl.startswith(head))
+
+    def testSetSender(self):
+        self.assertEqual(self.mail.get_sender(), 'test')
+        self.mail.set_sender('test2')
+        self.assertEqual(self.mail.get_sender(), 'test2')
+
+    def testGetSender(self):
+        pass # see testSetSender()
+
+    def checkSetAddr(self, set_method, get_method):
+        set_method('test')
+        self.assertEqual(get_method(), ['test'])
+
+        set_method(['test1', 'test2'])
+        self.assertEqual(get_method(), ['test1', 'test2'])
+
+        set_method('test1, test2')
+        self.assertEqual(get_method(), ['test1', 'test2'])
+
+    def checkAddAddr(self, add_method, get_method):
+        add_method(['test1', 'test2'])
+        self.assertEqual(get_method(), ['test1', 'test2'])
+
+        add_method('test3, test4')
+        self.assertEqual(get_method(), ['test1', 'test2', 'test3', 'test4'])
+
+    def testSetTo(self):
+        self.checkSetAddr(self.mail.set_to, self.mail.get_to)
+
+    def testAddTo(self):
+        self.checkAddAddr(self.mail.add_to, self.mail.get_to)
+
+    def testGetTo(self):
+        pass # see testSetTo()
+
+    def testSetCc(self):
+        self.checkSetAddr(self.mail.set_cc, self.mail.get_cc)
+
+    def testAddCc(self):
+        self.checkAddAddr(self.mail.add_cc, self.mail.get_cc)
+
+    def testGetCc(self):
+        pass # see testSetCc()
+
+    def testSetBcc(self):
+        self.checkSetAddr(self.mail.set_bcc, self.mail.get_bcc)
+
+    def testAddBcc(self):
+        self.checkAddAddr(self.mail.add_bcc, self.mail.get_bcc)
+
+    def testGetBcc(self):
+        pass # see testSetBcc()
+
+    def testGetReceipients(self):
+        self.mail.set_to('test1')
+        self.mail.set_cc('test2')
+        self.mail.set_bcc('test3')
+        self.assertEqual(self.mail.get_receipients(),
+                         ['test1', 'test2', 'test3'])
+
+    def testSetSubject(self):
+        self.assertEqual(self.mail.get_subject(), '')
+        self.mail.set_subject('test')
+        self.assertEqual(self.mail.get_subject(), 'test')
+
+    def testGetSubject(self):
+        pass # see testSetSubject()
+
+    def testSetBody(self):
+        self.assertEqual(self.mail.get_body(), '\n')
+        self.mail.set_body('test')
+        self.assertEqual(self.mail.get_body(), 'test\n')
+
+    def testGetBody(self):
+        pass # see testSetBody()
+
+    def testGetSmtpHeader(self):
+        pass # see testSetFromTemplateString()
+
+    def testGetSmtpMail(self):
+        pass # see testSetFromTemplateString()
+
+class mailTest(unittest.TestCase):
+    CORRELATE = Exscript.util.mail
+
+    def checkResult(self, mail):
+        self.assert_(isinstance(mail, Mail))
+
+        # Remove the "From:" line.
+        result   = mail.get_smtp_mail().split('\n', 1)[1].strip()
+        expected = smtp.split('\n', 1)[1].strip()
+
+        # Compare the results.
+        for key, value in vars.iteritems():
+            expected = expected.replace('{' + key + '}', value)
+        self.assertEqual(result, expected)
+
+    def testFromTemplateString(self):
+        from Exscript.util.mail import from_template_string
+        mail = from_template_string(template, **vars)
+        self.checkResult(mail)
+
+    def testFromTemplate(self):
+        from Exscript.util.mail import from_template
+        tmpfile = tempfile.NamedTemporaryFile()
+        tmpfile.write(template)
+        tmpfile.flush()
+        mail = from_template(tmpfile.name, **vars)
+        tmpfile.close()
+        self.checkResult(mail)
+
+def suite():
+    mail_cls    = unittest.TestLoader().loadTestsFromTestCase(MailTest)
+    mail_module = unittest.TestLoader().loadTestsFromTestCase(mailTest)
+    return unittest.TestSuite([mail_cls, mail_module])
+if __name__ == '__main__':
+    unittest.TextTestRunner(verbosity = 2).run(suite())
