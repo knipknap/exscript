40a55a087cfc4840588983ca938ebc7c52f8fd7a
Samuel Abels <knipknap@gmail.com>
2010-04-06 17:00:33 +0200

    exscriptd: some cleanups in the test service.

--- tests/Exscriptd/testservice/test.py
+++ tests/Exscriptd/testservice/test.py
@@ -1,4 +1,8 @@
-print "Hello Service!"
+print "Hello Test-Service!"
 
-def run(conn, order):
-    print "Hello from run()!", order.id
+def run(order, conn):
+    """
+    Called whenever a host that is associated with an order was contacted.
+    """
+    hostname = conn.get_host().get_name()
+    print "Hello from test-service run()!", order.id, hostname
