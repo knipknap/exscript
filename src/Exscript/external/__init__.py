aaa9b7edfb2c4536285dce052a17453912fec2a0
Samuel Abels <knipknap@gmail.com>
2010-04-19 14:17:42 +0200

    fix: hide deprecation warnings caused by paramiko.

--- src/Exscript/external/__init__.py
+++ src/Exscript/external/__init__.py
@@ -1,5 +1,7 @@
-import os, sys
+import os, sys, warnings
 sys.path.insert(0, os.path.dirname(__file__))
-import paramiko
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore", category = DeprecationWarning)
+    import paramiko
 import SpiffSignal
 sys.path.pop(0)
