1885a4f3f5a522793888fe12330c7520f2456421
Samuel Abels <knipknap@gmail.com>
2010-11-23 13:25:46 +0100

    get rid of the SpiffSignal dependency.

--- src/Exscript/external/__init__.py
+++ src/Exscript/external/__init__.py
@@ -3,5 +3,4 @@ sys.path.insert(0, os.path.dirname(__file__))
 with warnings.catch_warnings():
     warnings.filterwarnings("ignore", category = DeprecationWarning)
     import paramiko
-import SpiffSignal
 sys.path.pop(0)
