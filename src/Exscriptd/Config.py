ab66f7907bbecb9430429d198f29419c08e20758
Samuel Abels <knipknap@gmail.com>
2011-01-07 10:14:06 +0100

    Exscriptd.Config: clearly separate public from private methods.

--- src/Exscriptd/Config.py
+++ src/Exscriptd/Config.py
@@ -35,10 +35,146 @@ class Config(ConfigReader):
         self.daemons          = {}
         self.account_managers = {}
         self.queues           = {}
+        self.databases        = {}
         self.cfg_dir          = cfg_dir
         self.service_dir      = os.path.join(cfg_dir, 'services')
         filename              = os.path.join(cfg_dir, 'main.xml')
         ConfigReader.__init__(self, filename, resolve_variables)
+
+    def _init_account_pool_from_name(self, name):
+        accounts = []
+        element  = self.cfgtree.find('account-pool[@name="%s"]' % name)
+        for child in element.iterfind('account'):
+            user     = child.find('user').text
+            password = child.find('password').text
+            accounts.append(Account(user, base64.decodestring(password)))
+        return accounts
+
+    def _init_account_manager_from_name(self, name):
+        if name in self.account_managers:
+            return self.account_managers[name]
+        accounts = self._init_account_pool_from_name(name)
+        manager  = AccountManager(accounts)
+        self.account_managers[name] = manager
+        return manager
+
+    def _init_queue_from_name(self, name, logdir):
+        if name in self.queues:
+            return self.queues[name]
+
+        # Create the queue first.
+        element     = self.cfgtree.find('queue[@name="%s"]' % name)
+        max_threads = element.find('max-threads').text
+        delete_logs = element.find('delete-logs') is not None
+        queue       = Queue(verbose     = 0,
+                            max_threads = max_threads,
+                            logdir      = logdir,
+                            delete_logs = delete_logs)
+
+        # Add some accounts, if any.
+        account_pool = element.find('account-pool')
+        if account_pool is not None:
+            manager = self._init_account_manager_from_name(account_pool.text)
+            queue.account_manager = manager
+
+        self.queues[name] = queue
+        return queue
+
+    def _init_database_from_dbn(self, dbn):
+        from sqlalchemy import create_engine
+        #print 'Creating database connection for', dbn
+        engine  = create_engine(dbn)
+        db      = OrderDB(engine)
+        #print 'Initializing database tables...'
+        db.install()
+        return db
+
+    def _init_http_daemon(self, element):
+        # Init the database for the daemon first, then
+        # create the daemon (this does not start it).
+        name    = element.get('name')
+        address = element.find('address').text or ''
+        port    = int(element.find('port').text)
+        db_elem = element.find('database')
+        logdir  = element.find('logdir').text
+        if db_elem is None:
+            db = self.database_from_dbn(':memory:')
+        else:
+            db = self.get_database_from_name(db_elem.text)
+        if not os.path.isdir(logdir):
+            os.makedirs(logdir)
+        daemon  = HTTPDaemon(name,
+                             address,
+                             port,
+                             database = db,
+                             logdir   = logdir)
+
+        # Add some accounts, if any.
+        account_pool = element.find('account-pool')
+        for account in self._init_account_pool_from_name(account_pool.text):
+            daemon.add_account(account)
+
+        return daemon
+
+    def _init_daemons(self):
+        for element in self.cfgtree.iterfind('daemon'):
+            name = element.get('name')
+            self.get_daemon_from_name(name)
+
+    def _get_service_files(self):
+        """
+        Searches the config directory for service configuration files,
+        and returns a list of path names.
+        """
+        files = []
+        for file in os.listdir(self.service_dir):
+            config_dir = os.path.join(self.service_dir, file)
+            if not os.path.isdir(config_dir):
+                continue
+            config_file = os.path.join(config_dir, 'config.xml')
+            if not os.path.isfile(config_file):
+                continue
+            files.append(config_file)
+        return files
+
+    def _get_service_file_from_name(self, name):
+        """
+        Searches the config directory for service configuration files,
+        and returns the name of the first file that defines a service
+        with the given name.
+        """
+        for file in self._get_service_files():
+            xml     = etree.parse(file)
+            element = xml.find('service[@name="%s"]' % name)
+            if element is not None:
+                return file
+        return None
+
+    def _init_service(self, filename):
+        service_dir = os.path.dirname(filename)
+        cfgtree     = ConfigReader(filename).cfgtree
+        for element in cfgtree.iterfind('service'):
+            name = element.get('name')
+            print 'Loading service "%s"...' % name,
+
+            module      = element.find('module').text
+            daemon_name = element.find('daemon').text
+            daemon      = self.get_daemon_from_name(daemon_name)
+            queue_elem  = element.find('queue')
+            queue_name  = queue_elem is not None and queue_elem.text
+            logdir      = daemon.get_logdir()
+            queue       = self._init_queue_from_name(queue_name, logdir)
+            service     = PythonService(daemon,
+                                        name,
+                                        module,
+                                        service_dir,
+                                        queue = queue)
+            daemon.add_service(name, service)
+            print 'done.'
+
+    def _init_services(self):
+        for file in self._get_service_files():
+            service = self._init_service(file)
 
     def has_account_pool(self, name):
         return self.cfgtree.find('account-pool[@name="%s"]' % name) is not None
@@ -60,47 +196,8 @@ class Config(ConfigReader):
 
         self.save()
 
-    def init_account_pool_from_name(self, name):
-        accounts = []
-        element  = self.cfgtree.find('account-pool[@name="%s"]' % name)
-        for child in element.iterfind('account'):
-            user     = child.find('user').text
-            password = child.find('password').text
-            accounts.append(Account(user, base64.decodestring(password)))
-        return accounts
-
-    def init_account_manager_from_name(self, name):
-        if name in self.account_managers:
-            return self.account_managers[name]
-        accounts = self.init_account_pool_from_name(name)
-        manager  = AccountManager(accounts)
-        self.account_managers[name] = manager
-        return manager
-
     def has_queue(self, name):
         return self.cfgtree.find('queue[@name="%s"]' % name) is not None
-
-    def init_queue_from_name(self, name, logdir):
-        if name in self.queues:
-            return self.queues[name]
-
-        # Create the queue first.
-        element     = self.cfgtree.find('queue[@name="%s"]' % name)
-        max_threads = element.find('max-threads').text
-        delete_logs = element.find('delete-logs') is not None
-        queue       = Queue(verbose     = 0,
-                            max_threads = max_threads,
-                            logdir      = logdir,
-                            delete_logs = delete_logs)
-
-        # Add some accounts, if any.
-        account_pool = element.find('account-pool')
-        if account_pool is not None:
-            manager = self.init_account_manager_from_name(account_pool.text)
-            queue.account_manager = manager
-
-        self.queues[name] = queue
-        return queue
 
     def has_database(self, name):
         return self.cfgtree.find('database[@name="%s"]' % name) is not None
@@ -124,61 +221,14 @@ class Config(ConfigReader):
         self.save()
         return True
 
-    def init_database_from_dbn(self, dbn):
-        from sqlalchemy import create_engine
-        #print 'Creating database connection for', dbn
-        engine  = create_engine(dbn)
-        db      = OrderDB(engine)
-        #print 'Initializing database tables...'
-        db.install()
-        return db
-
-    def init_database_from_name(self, name):
+    def get_database_from_name(self, name):
+        if name in self.databases:
+            return self.databases[name]
         element = self.cfgtree.find('database[@name="%s"]' % name)
         dbn     = element.find('dbn').text
-        return self.init_database_from_dbn(dbn)
-
-    def load_service(self, filename):
-        service_dir = os.path.dirname(filename)
-        cfgtree     = ConfigReader(filename).cfgtree
-        for element in cfgtree.iterfind('service'):
-            name = element.get('name')
-            print 'Loading service "%s"...' % name,
-
-            module      = element.find('module').text
-            daemon_name = element.find('daemon').text
-            daemon      = self.init_daemon_from_name(daemon_name)
-            queue_elem  = element.find('queue')
-            queue_name  = queue_elem is not None and queue_elem.text
-            logdir      = daemon.get_logdir()
-            queue       = self.init_queue_from_name(queue_name, logdir)
-            service     = PythonService(daemon,
-                                        name,
-                                        module,
-                                        service_dir,
-                                        queue = queue)
-            daemon.add_service(name, service)
-            print 'done.'
-
-    def get_service_files(self):
-        files = []
-        for file in os.listdir(self.service_dir):
-            config_dir = os.path.join(self.service_dir, file)
-            if not os.path.isdir(config_dir):
-                continue
-            config_file = os.path.join(config_dir, 'config.xml')
-            if not os.path.isfile(config_file):
-                continue
-            files.append(config_file)
-        return files
-
-    def get_service_file_from_name(self, name):
-        for file in self.get_service_files():
-            xml     = etree.parse(file)
-            element = xml.find('service[@name="%s"]' % name)
-            if element is not None:
-                return file
-        return None
+        db      = self._init_database_from_dbn(dbn)
+        self.databases[name] = db
+        return db
 
     def add_queue(self,
                   queue_name,
@@ -200,7 +250,7 @@ class Config(ConfigReader):
             queue_elem.remove(acc_elem)
         elif account_pool is not None:
             try:
-                self.init_account_pool_from_name(account_pool)
+                self._init_account_pool_from_name(account_pool)
             except AttributeError:
                 raise Exception('no such account pool: %s' % account_pool)
 
@@ -228,14 +278,14 @@ class Config(ConfigReader):
         return True
 
     def has_service(self, service_name):
-        return self.get_service_file_from_name(service_name) is not None
+        return self._get_service_file_from_name(service_name) is not None
 
     def add_service(self,
                     service_name,
                     module_name = None,
                     daemon_name = None,
                     queue_name  = None):
-        pathname = self.get_service_file_from_name(service_name)
+        pathname = self._get_service_file_from_name(service_name)
         changed  = False
 
         if not pathname:
@@ -293,7 +343,7 @@ class Config(ConfigReader):
         return True
 
     def _get_service_var_elem(self, service_name):
-        pathname = self.get_service_file_from_name(service_name)
+        pathname = self._get_service_file_from_name(service_name)
         doc      = etree.parse(pathname)
         xml      = doc.getroot()
         return pathname, xml, xml.find('variables')
@@ -312,10 +362,6 @@ class Config(ConfigReader):
         if elem is not None:
             var_elem.remove(elem)
         self._write_xml(xml, path)
-
-    def load_services(self):
-        for file in self.get_service_files():
-            service = self.load_service(file)
 
     def has_daemon(self, name):
         return self.cfgtree.find('daemon[@name="%s"]' % name) is not None
@@ -352,34 +398,7 @@ class Config(ConfigReader):
         self.save()
         return changed
 
-    def init_http_daemon(self, element):
-        # Init the database for the daemon first, then
-        # create the daemon (this does not start it).
-        name    = element.get('name')
-        address = element.find('address').text or ''
-        port    = int(element.find('port').text)
-        db_elem = element.find('database')
-        logdir  = element.find('logdir').text
-        if db_elem is None:
-            db = self.database_from_dbn(':memory:')
-        else:
-            db = self.init_database_from_name(db_elem.text)
-        if not os.path.isdir(logdir):
-            os.makedirs(logdir)
-        daemon  = HTTPDaemon(name,
-                             address,
-                             port,
-                             database = db,
-                             logdir   = logdir)
-
-        # Add some accounts, if any.
-        account_pool = element.find('account-pool')
-        for account in self.init_account_pool_from_name(account_pool.text):
-            daemon.add_account(account)
-
-        return daemon
-
-    def init_daemon_from_name(self, name):
+    def get_daemon_from_name(self, name):
         if name in self.daemons:
             return self.daemons[name]
 
@@ -387,13 +406,14 @@ class Config(ConfigReader):
         element = self.cfgtree.find('daemon[@name="%s"]' % name)
         type    = element.get('type')
         if type == 'http':
-            daemon = self.init_http_daemon(element)
+            daemon = self._init_http_daemon(element)
         else:
             raise Exception('No such daemon type: %s' % type)
 
         self.daemons[name] = daemon
         return daemon
 
-    def init_daemons(self):
-        self.load_services()
+    def get_daemons(self):
+        self._init_daemons()
+        self._init_services()
         return self.daemons.values()
