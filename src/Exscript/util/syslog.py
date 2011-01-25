eb712d30a6231b416e33d02a125daddf5322d51e
Samuel Abels <knipknap@gmail.com>
2011-01-25 13:55:37 +0100

    Add API docs for the Exscript.util.syslog module.

--- src/Exscript/util/syslog.py
+++ src/Exscript/util/syslog.py
@@ -1,4 +1,22 @@
-import imp, socket
+# Copyright (C) 2007-2010 Samuel Abels.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+"""
+Send messages to a syslog server.
+"""
+import imp
+import socket
 
 # This way of loading a module prevents Python from looking in the
 # current directory. (We need to avoid it due to the syslog module
@@ -37,4 +55,3 @@ def netlog(message,
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.sendto(data, (host, port))
     sock.close()
-
