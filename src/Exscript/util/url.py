0f56751a677f6002d1e38016402693603cb41132
Samuel Abels <knipknap@gmail.com>
2011-02-04 14:37:15 +0100

    Exscript.util.url: essentially rewrite the mess that was _urlparse_qs().

--- src/Exscript/util/url.py
+++ src/Exscript/util/url.py
@@ -53,7 +53,7 @@ def _unquote(string):
             result[i] = unichr(int(item[:2], 16)) + item[2:]
     return ''.join(result)
 
-def _urlparse_qs(url, keep_blank_values=0, strict_parsing=0):
+def _urlparse_qs(url):
     """
     Parse a URL query string and return the components as a dictionary.
 
@@ -64,47 +64,26 @@ def _urlparse_qs(url, keep_blank_values=0, strict_parsing=0):
     Arguments:
 
       - url: URL with query string to be parsed
-
-      - keep_blank_values: flag indicating whether blank values in
-        URL encoded queries should be treated as blank strings.
-        A true value indicates that blanks should be retained as
-        blank strings.  The default false value indicates that
-        blank values are to be ignored and treated as if they were
-        not included.
-
-      - strict_parsing: flag indicating what to do with parsing errors.
-        If false (the default), errors are silently ignored.
-        If true, errors raise a ValueError exception.
     """
-
+    # Extract the query part from the URL.
     scheme, netloc, url, params, querystring, fragment = urlparse(url)
 
+    # Split the query into name/value pairs.
     pairs = [s2 for s1 in querystring.split('&') for s2 in s1.split(';')]
-    query = []
-    for name_value in pairs:
-        if not name_value and not strict_parsing:
-            continue
-        nv = name_value.split('=', 1)
-        if len(nv) != 2:
-            if strict_parsing:
-                raise ValueError, "bad query field: %r" % (name_value,)
-            # Handle case of a control-name with no equal sign
-            if keep_blank_values:
-                nv.append('')
-            else:
-                continue
-        if len(nv[1]) or keep_blank_values:
-            name = _unquote(nv[0].replace('+', ' '))
-            value = _unquote(nv[1].replace('+', ' '))
-            query.append((name, value))
 
-    dict = {}
-    for name, value in query:
-        if name in dict:
-            dict[name].append(value)
-        else:
-            dict[name] = [value]
-    return dict
+    # Split the name/value pairs.
+    result = defaultdict(list)
+    for name_value in pairs:
+        pair = name_value.split('=', 1)
+        if len(pair) != 2:
+            continue
+
+        if len(pair[1]) > 0:
+            name  = _unquote(pair[0].replace('+', ' '))
+            value = _unquote(pair[1].replace('+', ' '))
+            result[name].append(value)
+
+    return result
 
 ###############################################################
 # public api
