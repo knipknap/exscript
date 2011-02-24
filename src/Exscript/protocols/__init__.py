1f686cfcd3a2f69ddd9c3e5098a00dc5e36833d3
Samuel Abels <knipknap@gmail.com>
2011-02-24 08:37:28 +0100

    add Exscript.protocols.connect().

--- src/Exscript/protocols/__init__.py
+++ src/Exscript/protocols/__init__.py
@@ -12,6 +12,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+from Exscript.util.url           import Url
 from Exscript.protocols.Protocol import Protocol
 from Exscript.protocols.Telnet   import Telnet
 from Exscript.protocols.SSH2     import SSH2
@@ -40,6 +41,32 @@ def create_protocol(name, **kwargs):
     if not cls:
         raise ValueError('Unsupported protocol "%s".' % name)
     return cls(**kwargs)
+
+def connect(uri, default_protocol = 'telnet', **kwargs):
+    """
+    Parses the given URL-formatted hostname using L{Exscript.util.url},
+    creates an instance of the protocol, and calls L{Protocol.connect()}
+    on it. If the URL contains a username, L{Protocol.login()} is also
+    called.
+
+    @type  uri: str
+    @param uri: The URL-formatted hostname.
+    @type  default_protocol: str
+    @param default_protocol: Protocol that is used if the URL specifies none.
+    @type  kwargs: dict
+    @param kwargs: Passed to the protocol constructor.
+    """
+    url  = Url.from_string(uri, default_protocol)
+    conn = create_protocol(url.protocol, **kwargs)
+    conn.connect(url.hostname)
+
+    if url.username is not None \
+       or url.password1 is not None \
+       or url.password2:
+        account = Account(url.username, url.password1, url.password2)
+        conn.login(account)
+
+    return conn
 
 import inspect 
 __all__ = [name for name, obj in locals().items()
