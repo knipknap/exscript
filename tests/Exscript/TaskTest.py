c78f299c132039700c9316f03ffd3b77c8ee5315
Samuel Abels <knipknap@gmail.com>
2011-05-25 11:18:03 +0200

    add a test for Exscript.Task.

---
+++ tests/Exscript/TaskTest.py
@@ -0,0 +1,47 @@
+import sys, unittest, re, os.path, warnings
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..', 'src'))
+
+warnings.simplefilter('ignore', DeprecationWarning)
+
+from Exscript.workqueue import WorkQueue
+from Exscript.Task import Task
+
+class TaskTest(unittest.TestCase):
+    CORRELATE = Task
+
+    def setUp(self):
+        self.wq = WorkQueue()
+
+    def tearDown(self):
+        self.wq.shutdown(True)
+
+    def testConstructor(self):
+        task = Task(self.wq)
+
+    def testIsCompleted(self):
+        task = Task(self.wq)
+        task.add_job_id(123)
+        task.wait() # Returns immediately because the id is not known.
+
+    def testWait(self):
+        task = Task(self.wq)
+        self.assertEqual(task.is_completed(), True)
+
+        job1 = object()
+        job2 = object()
+        task.add_job_id(id(job1))
+        task.add_job_id(id(job2))
+        self.assertEqual(task.is_completed(), False)
+
+        self.wq.job_succeeded_event(job1)
+        self.assertEqual(task.is_completed(), False)
+        self.wq.job_succeeded_event(job2)
+        self.assertEqual(task.is_completed(), True)
+
+    def testAddJobId(self):
+        self.testWait()
+
+def suite():
+    return unittest.TestLoader().loadTestsFromTestCase(TaskTest)
+if __name__ == '__main__':
+    unittest.TextTestRunner(verbosity = 2).run(suite())
