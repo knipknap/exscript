9ec27202cacce045cd2dc7d775c9054b91e38a8a
Samuel Abels <knipknap@gmail.com>
2011-06-14 16:04:06 +0200

    Add Exscript.workqueue.Pipeline.

---
+++ src/Exscript/workqueue/Pipeline.py
@@ -0,0 +1,253 @@
+# Copyright (C) 2007-2010 Samuel Abels.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+from itertools import chain
+from collections import deque
+from multiprocessing import Condition, RLock
+
+class Pipeline(object):
+    """
+    A collection that is similar to Python's Queue object, except
+    it also tracks items that are currently sleeping or in progress.
+    """
+    def __init__(self, max_working = 1):
+        self.condition   = Condition(RLock())
+        self.max_working = max_working
+        self.running     = True
+        self.paused      = False
+        self.queue       = None
+        self.force       = None
+        self.sleeping    = None
+        self.working     = None
+        self.all         = None # for performance reasons
+        self.clear()
+
+    def __len__(self):
+        with self.condition:
+            return len(self.all)
+
+    def __contains__(self, item):
+        with self.condition:
+            return id(item) in self.all
+
+    def find(self, callable):
+        """
+        Returns the first item for which the given callable returns True.
+        Returns None if no such item was found.
+        """
+        with self.condition:
+            for item in chain(self.force, self.queue, self.working):
+                if callable(item):
+                    return item
+        return None
+
+    def has_id(self, item_id):
+        """
+        Returns True if the queue contains an item with the given id.
+        """
+        return item_id in self.all
+
+    def task_done(self, item):
+        with self.condition:
+            self.working.remove(item)
+            self.all.remove(id(item))
+            self.condition.notify_all()
+
+    def replace(self, old, new):
+        with self.condition:
+            if old in self.queue:
+                self.queue.remove(old)
+                self.queue.add(new)
+            if old in self.force:
+                self.force.remove(old)
+                self.force.add(new)
+            if old in self.sleeping:
+                self.sleeping.remove(old)
+                self.sleeping.add(new)
+            if old in self.working:
+                self.working.remove(old)
+                self.working.add(new)
+            self.all.remove(id(old))
+            self.all.add(id(new))
+            self.condition.notify_all()
+
+    def append(self, item):
+        with self.condition:
+            self.queue.append(item)
+            self.all.add(id(item))
+            self.condition.notify_all()
+
+    def appendleft(self, item, force = False):
+        with self.condition:
+            if force:
+                self.force.append(item)
+            else:
+                self.queue.appendleft(item)
+            self.all.add(id(item))
+            self.condition.notify_all()
+
+    def prioritize(self, item, force = False):
+        """
+        Moves the item to the very left of the queue.
+        """
+        with self.condition:
+            # If the job is already running (or about to be forced),
+            # there is nothing to be done.
+            if item in self.working or item in self.force:
+                return
+            self.queue.remove(item)
+            self.appendleft(item, force)
+            self.condition.notify_all()
+
+    def clear(self):
+        with self.condition:
+            self.queue    = deque()
+            self.force    = deque()
+            self.sleeping = set()
+            self.working  = set()
+            self.all      = set()
+            self.condition.notify_all()
+
+    def stop(self):
+        """
+        Force the next() method to return while in another thread.
+        The return value of next() will be None.
+        """
+        with self.condition:
+            self.running = False
+            self.condition.notify_all()
+
+    def pause(self):
+        with self.condition:
+            self.paused = True
+            self.condition.notify_all()
+
+    def unpause(self):
+        with self.condition:
+            self.paused = False
+            self.condition.notify_all()
+
+    def sleep(self, item):
+        assert id(item) in self.all
+        with self.condition:
+            self.sleeping.add(item)
+            self.condition.notify_all()
+
+    def wake(self, item):
+        assert id(item) in self.all
+        assert item in self.sleeping
+        with self.condition:
+            self.sleeping.remove(item)
+            self.condition.notify_all()
+
+    def wait_for_id(self, item_id):
+        with self.condition:
+            while self.has_id(item_id):
+                self.condition.wait()
+
+    def wait(self):
+        """
+        Waits for all currently running tasks to complete.
+        """
+        with self.condition:
+            while self.working:
+                self.condition.wait()
+
+    def wait_all(self):
+        """
+        Waits for all queued and running tasks to complete.
+        """
+        with self.condition:
+            while len(self) > 0:
+                self.condition.wait()
+
+    def with_lock(self, function, *args, **kwargs):
+        with self.condition:
+            return function(self, *args, **kwargs)
+
+    def set_max_working(self, max_working):
+        with self.condition:
+            self.max_working = int(max_working)
+            self.condition.notify_all()
+
+    def get_max_working(self):
+        return self.max_working
+
+    def get_working(self):
+        return list(self.working)
+
+    def is_running(self):
+        return self.running
+
+    def _popleft_sleeping(self):
+        sleeping = []
+        while True:
+            try:
+                node = self.queue[0]
+            except IndexError:
+                break
+            if node not in self.sleeping:
+                break
+            sleeping.append(node)
+            self.queue.popleft()
+        return sleeping
+
+    def _get_next(self):
+        # We need to leave sleeping items in the queue because else we
+        # would not know their original position after they wake up.
+        # So we need to temporarily remove sleeping items from the top of
+        # the queue here.
+        sleeping = self._popleft_sleeping()
+
+        # Get the first non-sleeping item from the queue.
+        try:
+            next = self.queue.popleft()
+        except IndexError:
+            next = None
+
+        # Re-insert sleeping items.
+        self.queue.extendleft(sleeping)
+        return next
+
+    def next(self):
+        with self.condition:
+            while self.running:
+                if self.paused:
+                    self.condition.wait()
+                    continue
+
+                # Wait until enough slots are available.
+                if len(self.working) - \
+                   len(self.sleeping) - \
+                   len(self.force) >= self.max_working:
+                    self.condition.wait()
+                    continue
+
+                # Forced items are returned regardless of how many tasks
+                # are already working.
+                try:
+                    next = self.force.popleft()
+                except IndexError:
+                    pass
+                else:
+                    self.working.add(next)
+                    return next
+
+                # Return the first non-sleeping task.
+                next = self._get_next()
+                if next is None:
+                    self.condition.wait()
+                    continue
+                self.working.add(next)
+                return next
---
+++ tests/Exscript/workqueue/PipelineTest.py
@@ -0,0 +1,372 @@
+import sys, unittest, re, os.path, threading, time
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..', '..', 'src'))
+
+import time
+from threading import Thread
+from multiprocessing import Value
+from Exscript.workqueue import Pipeline
+
+class PipelineTest(unittest.TestCase):
+    CORRELATE = Pipeline
+
+    def setUp(self):
+        self.pipeline = Pipeline()
+
+    def testConstructor(self):
+        self.assertEqual(self.pipeline.get_max_working(), 1)
+        pipeline = Pipeline(max_working = 10)
+        self.assertEqual(pipeline.get_max_working(), 10)
+
+    def testLen(self):
+        self.assertEqual(len(self.pipeline), 0)
+
+    def testContains(self):
+        item1 = object()
+        item2 = object()
+        self.assert_(item1 not in self.pipeline)
+        self.assert_(item2 not in self.pipeline)
+
+        self.pipeline.append(item1)
+        self.assert_(item1 in self.pipeline)
+        self.assert_(item2 not in self.pipeline)
+
+        self.pipeline.append(item2)
+        self.assert_(item1 in self.pipeline)
+        self.assert_(item2 in self.pipeline)
+
+    def testFind(self):
+        item1       = object()
+        item2       = object()
+        find_none   = lambda x: False
+        find_all    = lambda x: True
+        find_second = lambda x: x == item2
+        self.assertEqual(self.pipeline.find(find_none), None)
+        self.assertEqual(self.pipeline.find(find_all), None)
+        self.assertEqual(self.pipeline.find(find_second), None)
+
+        self.pipeline.append(item1)
+        self.pipeline.append(item2)
+        self.assertEqual(self.pipeline.find(find_none), None)
+        self.assertEqual(self.pipeline.find(find_all), item1)
+        self.assertEqual(self.pipeline.find(find_second), item2)
+
+    def testHasId(self):
+        item1 = object()
+        item2 = object()
+        self.assertEqual(self.pipeline.has_id(id(item1)), False)
+        self.assertEqual(self.pipeline.has_id(id(item2)), False)
+
+        self.pipeline.append(item1)
+        self.assertEqual(self.pipeline.has_id(id(item1)), True)
+        self.assertEqual(self.pipeline.has_id(id(item2)), False)
+
+        self.pipeline.append(item2)
+        self.assertEqual(self.pipeline.has_id(id(item1)), True)
+        self.assertEqual(self.pipeline.has_id(id(item2)), True)
+
+    def testTaskDone(self):
+        self.testNext()
+
+    def testReplace(self):
+        item1 = object()
+        item2 = object()
+        self.pipeline.append(item1)
+        self.pipeline.next()
+        self.assert_(item1 in self.pipeline.get_working())
+        self.pipeline.replace(item1, item2)
+        self.assert_(item1 not in self.pipeline.get_working())
+        self.assert_(item2 in self.pipeline.get_working())
+
+    def testAppend(self):
+        self.testContains()
+
+    def testAppendleft(self):
+        item1      = object()
+        item2      = object()
+        item3      = object()
+        item4      = object()
+        find_first = lambda x: True
+        self.assertEqual(self.pipeline.find(find_first), None)
+
+        self.pipeline.append(item1)
+        self.pipeline.append(item2)
+        self.assertEqual(self.pipeline.find(find_first), item1)
+
+        self.pipeline.appendleft(item3)
+        self.assertEqual(self.pipeline.find(find_first), item3)
+
+        self.pipeline.appendleft(item4, True)
+        self.assertEqual(self.pipeline.find(find_first), item4)
+
+    def testPrioritize(self):
+        item1      = object()
+        item2      = object()
+        find_first = lambda x: True
+        self.assertEqual(self.pipeline.find(find_first), None)
+
+        self.pipeline.append(item1)
+        self.pipeline.append(item2)
+        self.assertEqual(self.pipeline.find(find_first), item1)
+
+        self.pipeline.prioritize(item2)
+        self.assertEqual(self.pipeline.find(find_first), item2)
+        self.pipeline.prioritize(item2)
+        self.assertEqual(self.pipeline.find(find_first), item2)
+
+        self.pipeline.prioritize(item1, True)
+        self.assertEqual(self.pipeline.find(find_first), item1)
+        self.pipeline.prioritize(item1, True)
+        self.assertEqual(self.pipeline.find(find_first), item1)
+
+    def testClear(self):
+        self.testAppendleft()
+        self.assertEqual(len(self.pipeline), 4)
+        self.pipeline.clear()
+        self.assertEqual(len(self.pipeline), 0)
+
+    def testStop(self):
+        self.assertEqual(self.pipeline.get_max_working(), 1)
+        item1 = object()
+        item2 = object()
+        self.pipeline.append(item1)
+        self.pipeline.append(item2)
+        self.assertEqual(len(self.pipeline), 2)
+
+        thread_completed = Value('i', 0)
+        class deadlock_until_stop(Thread):
+            def run(inner_self):
+                self.assertEqual(self.pipeline.next(), item1)
+                self.assertEqual(self.pipeline.next(), None) # ***
+                thread_completed.value = 1
+
+        thread = deadlock_until_stop()
+        thread.daemon = True
+        thread.start()
+
+        time.sleep(0.5) # Hack: Wait until the thread has reached "***"
+
+        self.assertEqual(thread_completed.value, 0)
+        self.pipeline.stop()
+        thread.join()
+        self.assertEqual(thread_completed.value, 1)
+
+    def testPause(self):
+        item1 = object()
+        self.pipeline.append(item1)
+        self.pipeline.pause()
+
+        class complete_all(Thread):
+            def run(inner_self):
+                while True:
+                    task = self.pipeline.next()
+                    if task is None:
+                        break
+                    self.pipeline.task_done(task)
+
+        thread = complete_all()
+        thread.daemon = True
+        thread.start()
+
+        time.sleep(.2) # hack: wait long enough for the task to complete.
+        self.assertEqual(len(self.pipeline), 1) # should not be completed.
+        self.pipeline.unpause()
+        self.pipeline.wait_all() # now it should not deadlock.
+
+    def testUnpause(self):
+        self.testPause()
+
+    def testSleep(self):
+        self.assertEqual(self.pipeline.get_max_working(), 1)
+        item1 = object()
+        item2 = object()
+        self.pipeline.append(item1)
+        self.pipeline.append(item2)
+        self.assertEqual(len(self.pipeline), 2)
+
+        self.assertEqual(self.pipeline.next(), item1)
+        self.assertEqual(len(self.pipeline), 2)
+        self.pipeline.sleep(item1)
+        self.assertEqual(len(self.pipeline), 2)
+
+        # This would normally deadlock if the job were not sleeping,
+        # because we have reached the max_working threshold.
+        self.assertEqual(self.pipeline.next(), item2)
+        self.assertEqual(len(self.pipeline), 2)
+
+        self.pipeline.wake(item1)
+        self.assertRaises(Exception, self.pipeline.wake, item2)
+        self.assertEqual(len(self.pipeline), 2)
+
+    def testWake(self):
+        self.testSleep()
+
+    def testWaitForId(self):
+        item1 = object()
+        item2 = object()
+        self.pipeline.append(item1)
+        self.pipeline.append(item2)
+
+        item = self.pipeline.next()
+        class complete_item(Thread):
+            def run(inner_self):
+                time.sleep(.1)
+                self.pipeline.task_done(item)
+        thread = complete_item()
+        thread.daemon = True
+        thread.start()
+
+        self.assertEqual(len(self.pipeline), 2)
+        self.pipeline.wait_for_id(id(item)) # Must not deadlock.
+        self.assertEqual(len(self.pipeline), 1)
+
+    def testWait(self):
+        item1 = object()
+        item2 = object()
+        self.pipeline.append(item1)
+        self.pipeline.append(item2)
+
+        self.assertEqual(len(self.pipeline), 2)
+        self.pipeline.wait()
+        self.assertEqual(len(self.pipeline), 2)
+
+        item = self.pipeline.next()
+        class complete_item(Thread):
+            def run(inner_self):
+                time.sleep(.1)
+                self.pipeline.task_done(item)
+        thread = complete_item()
+        thread.daemon = True
+        thread.start()
+
+        self.pipeline.wait() # Must not deadlock.
+        self.assertEqual(len(self.pipeline), 1)
+
+    def testWaitAll(self):
+        item1 = object()
+        item2 = object()
+        self.pipeline.append(item1)
+        self.pipeline.append(item2)
+
+        class complete_all(Thread):
+            def run(inner_self):
+                while True:
+                    task = self.pipeline.next()
+                    if task is None:
+                        break
+                    self.pipeline.task_done(task)
+        thread = complete_all()
+        thread.daemon = True
+        thread.start()
+
+        self.pipeline.wait_all() # Must not deadlock.
+        self.assertEqual(len(self.pipeline), 0)
+
+    def testWithLock(self):
+        result = self.pipeline.with_lock(lambda p, x: x, 'test')
+        self.assertEqual(result, 'test')
+
+    def testSetMaxWorking(self):
+        self.assertEqual(self.pipeline.get_max_working(), 1)
+        self.pipeline.set_max_working(2)
+        self.assertEqual(self.pipeline.get_max_working(), 2)
+
+    def testGetMaxWorking(self):
+        self.testSetMaxWorking()
+
+    def testGetWorking(self):
+        item = object()
+        self.pipeline.append(item)
+        self.assertEqual(self.pipeline.get_working(), [])
+        theitem = self.pipeline.next()
+        self.assertEqual(self.pipeline.get_working(), [item])
+        self.pipeline.task_done(theitem)
+
+    def testIsRunning(self):
+        self.assertEqual(self.pipeline.is_running(), True)
+        self.pipeline.stop()
+        self.assertEqual(self.pipeline.is_running(), False)
+
+    def testNext(self):
+        # Repeat with max_working set to a value larger than the
+        # queue length (i.e. no locking).
+        self.pipeline.set_max_working(4)
+        item1 = object()
+        item2 = object()
+        item3 = object()
+        item4 = object()
+        self.pipeline.append(item1)
+        self.pipeline.append(item2)
+        self.pipeline.appendleft(item3, True)
+        self.pipeline.appendleft(item4)
+
+        self.assertEqual(self.pipeline.next(), item3)
+        self.assertEqual(self.pipeline.next(), item4)
+        self.assertEqual(self.pipeline.next(), item1)
+        self.assertEqual(self.pipeline.next(), item2)
+        self.assert_(item1 in self.pipeline)
+        self.assert_(item2 in self.pipeline)
+        self.assert_(item3 in self.pipeline)
+        self.assert_(item4 in self.pipeline)
+        self.assertEqual(len(self.pipeline), 4)
+        self.pipeline.clear()
+        self.assertEqual(len(self.pipeline), 0)
+
+        # Repeat with max_working = 2.
+        self.pipeline.set_max_working(2)
+        self.pipeline.append(item1)
+        self.pipeline.append(item2)
+        self.pipeline.appendleft(item3, True)
+        self.pipeline.appendleft(item4)
+
+        self.assertEqual(self.pipeline.next(), item3)
+        self.assertEqual(self.pipeline.next(), item4)
+        self.assert_(item3 in self.pipeline)
+        self.assert_(item4 in self.pipeline)
+        self.pipeline.task_done(item4)
+        self.assert_(item4 not in self.pipeline)
+
+        self.assertEqual(self.pipeline.next(), item1)
+        self.assert_(item1 in self.pipeline)
+        self.pipeline.task_done(item3)
+        self.assert_(item3 not in self.pipeline)
+
+        self.assertEqual(self.pipeline.next(), item2)
+        self.assert_(item2 in self.pipeline)
+        self.pipeline.task_done(item2)
+        self.assert_(item2 not in self.pipeline)
+        self.pipeline.task_done(item1)
+        self.assert_(item1 not in self.pipeline)
+        self.assertEqual(len(self.pipeline), 0)
+
+        # Repeat with max_working = 1.
+        self.pipeline.set_max_working(1)
+        self.pipeline.append(item1)
+        self.pipeline.append(item2)
+        self.pipeline.appendleft(item3, True)
+        self.pipeline.appendleft(item4)
+
+        self.assertEqual(self.pipeline.next(), item3)
+        self.assert_(item3 in self.pipeline)
+        self.pipeline.task_done(item3)
+        self.assert_(item3 not in self.pipeline)
+
+        self.assertEqual(self.pipeline.next(), item4)
+        self.assert_(item4 in self.pipeline)
+        self.pipeline.task_done(item4)
+        self.assert_(item4 not in self.pipeline)
+
+        self.assertEqual(self.pipeline.next(), item1)
+        self.assert_(item1 in self.pipeline)
+        self.pipeline.task_done(item1)
+        self.assert_(item1 not in self.pipeline)
+
+        self.assertEqual(self.pipeline.next(), item2)
+        self.assert_(item2 in self.pipeline)
+        self.pipeline.task_done(item2)
+        self.assert_(item2 not in self.pipeline)
+        self.assertEqual(len(self.pipeline), 0)
+
+def suite():
+    return unittest.TestLoader().loadTestsFromTestCase(PipelineTest)
+if __name__ == '__main__':
+    unittest.TextTestRunner(verbosity = 2).run(suite())
