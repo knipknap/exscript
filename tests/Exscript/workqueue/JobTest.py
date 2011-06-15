36c1243eeff2bafeda1cccabc3db5b82f0e42378
Samuel Abels <knipknap@gmail.com>
2011-06-15 13:02:26 +0200

    improve test for Exscript.workqueue.Job.

--- tests/Exscript/workqueue/JobTest.py
+++ tests/Exscript/workqueue/JobTest.py
@@ -2,23 +2,22 @@ import sys, unittest, re, os.path, threading
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..', '..', 'src'))
 
 from multiprocessing import Pipe
-from Exscript.workqueue.Job import Thread, Process
+from Exscript.workqueue.Job import Thread, Process, Job
+from tempfile import NamedTemporaryFile
+from cPickle import dumps, loads
+
+def do_nothing(job):
+    pass
 
 class ThreadTest(unittest.TestCase):
     CORRELATE = Thread
 
-    def setUp(self):
-        self.condition = threading.Condition()
-        self.lock      = threading.Lock()
-        self.data      = {}
-        self.function  = lambda x: None
-
     def testConstructor(self):
-        job = self.CORRELATE(1, self.function, 'myaction', None)
-        self.assertEqual(self.function, job.function)
+        job = self.CORRELATE(1, do_nothing, 'myaction', None)
+        self.assertEqual(do_nothing, job.function)
 
     def testRun(self):
-        job = self.CORRELATE(1, self.function, 'myaction', None)
+        job = self.CORRELATE(1, do_nothing, 'myaction', None)
         to_child, to_self = Pipe()
         job.start(to_self)
         response = to_child.recv()
@@ -33,10 +32,26 @@ class ThreadTest(unittest.TestCase):
 class ProcessTest(ThreadTest):
     CORRELATE = Process
 
+class JobTest(unittest.TestCase):
+    def testConstructor(self):
+        job = Job(do_nothing, 'myaction', 1, 'foo')
+        self.assertEqual(job.name, 'myaction')
+        self.assertEqual(job.times, 1)
+        self.assertEqual(job.func, do_nothing)
+        self.assertEqual(job.data, 'foo')
+        self.assertEqual(job.child, None)
+
+    def testPickle(self):
+        job1 = Job(do_nothing, 'myaction', 1, None)
+        data = dumps(job1, -1)
+        job2 = loads(data)
+        self.assertEqual(job1.name, job2.name)
+
 def suite():
     loader = unittest.TestLoader()
     suite1 = loader.loadTestsFromTestCase(ThreadTest)
     suite2 = loader.loadTestsFromTestCase(ProcessTest)
-    return unittest.TestSuite((suite1, suite2))
+    suite3 = loader.loadTestsFromTestCase(JobTest)
+    return unittest.TestSuite((suite1, suite2, suite3))
 if __name__ == '__main__':
     unittest.TextTestRunner(verbosity = 2).run(suite())
