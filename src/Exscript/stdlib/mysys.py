3748fc0e75118fd888eb7b626bde333a9ef97dc6
Samuel Abels <knipknap@gmail.com>
2011-08-29 14:13:04 +0200

    fix: missing import.

--- src/Exscript/stdlib/mysys.py
+++ src/Exscript/stdlib/mysys.py
@@ -13,6 +13,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 import time
+import sys
 from subprocess import Popen, PIPE, STDOUT
 from Exscript.stdlib.util import secure_function
 
