2a1621296ff2885d4fa89ba6c9b0918cf29aaf02
Samuel Abels <knipknap@gmail.com>
2012-01-03 07:18:49 +0100

    Add memtest.py and memtest_host.py

---
+++ tests/Exscript/memtest_host.py
@@ -0,0 +1,12 @@
+# This script is not meant to provide a fully automated test, it's
+# merely a hack/starting point for investigating memory consumption
+# manually. The behavior also depends heavily on the version of meliae.
+from meliae import scanner, loader
+from Exscript import Account, Host
+
+hostlist = [Host(str(i)) for i in range(1, 10000)]
+#accountlist = [Account(str(i)) for i in range(1, 10000)]
+
+scanner.dump_all_objects('test.dump')
+om = loader.load('test.dump')
+print om.summarize()
