0f80a47ba2ee6dddec78b9a57c59b7a900d356a6
Samuel Abels <knipknap@gmail.com>
2012-02-07 15:56:59 +0100

    Exscript.util.event.Event.emit(): allow callbacks for terminating the event chain.

--- src/Exscript/util/event.py
+++ src/Exscript/util/event.py
@@ -145,16 +145,24 @@ class Event(object):
     def emit(self, *args, **kwargs):
         """
         Emits the signal, passing the given arguments to the callbacks.
+        If one of the callbacks returns a value other than None, no further
+        callbacks are invoked and the return value of the callback is
+        returned to the caller of emit().
 
         @type  args: tuple
         @param args: Optional arguments passed to the callbacks.
         @type  kwargs: dict
         @param kwargs: Optional keyword arguments passed to the callbacks.
+        @rtype:  object
+        @return: Returns None if all callbacks returned None. Returns
+                 the return value of the last invoked callback otherwise.
         """
         if self.hard_subscribers is not None:
             for callback, user_args, user_kwargs in self.hard_subscribers:
                 kwargs.update(user_kwargs)
-                callback(*args + user_args, **kwargs)
+                result = callback(*args + user_args, **kwargs)
+                if result is not None:
+                    return result
 
         if self.weak_subscribers is not None:
             for callback, user_args, user_kwargs in self.weak_subscribers:
@@ -172,7 +180,9 @@ class Event(object):
                 function = callback.get_function()
                 if function is None:
                     continue
-                function(*args + user_args, **kwargs)
+                result = function(*args + user_args, **kwargs)
+                if result is not None:
+                    return result
 
     def _try_disconnect(self, ref):
         """
