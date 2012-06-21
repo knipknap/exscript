f265aa84dec6cb926627b56faf87391d92fc7bae
Samuel Abels <knipknap@gmail.com>
2012-06-21 10:08:37 +0200

    fix demo report.py

--- demos/report/report.py
+++ demos/report/report.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 from Exscript                import Queue, Logger
+from Exscript.util.log       import log_to
 from Exscript.util.decorator import autologin
 from Exscript.util.file      import get_hosts_from_file, get_accounts_from_file
 from Exscript.util.report    import status, summarize
 
+logger = Logger() # Logs everything to memory.
+
+@log_to(logger)
 @autologin
 def do_something(conn):
     conn.execute('show ip int brie')
@@ -14,12 +18,8 @@ hosts    = get_hosts_from_file('hostlist.txt')
 
 # Run do_something on each of the hosts. The given accounts are used
 # round-robin. "verbose = 0" instructs the queue to not generate any
-# output on stdout. Using "logdir = ..." is equivalent to the following:
-#   logger = FileLogger(queue, 'my/logdir')
-# It instructs the queue to automatically log everything to the filesystem;
-# one file is created per host.
-queue  = Queue(verbose = 0, max_threads = 5, logdir = 'my/logdir/')
-logger = Logger(queue)          # Logs everything to memory.
+# output on stdout.
+queue = Queue(verbose = 0, max_threads = 5)
 queue.add_account(accounts)     # Adds one or more accounts.
 queue.run(hosts, do_something)  # Asynchronously enqueues all hosts.
 queue.shutdown()                # Waits until all hosts are completed.
