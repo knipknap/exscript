31d1fddfc47fcd9acedd07263521f7941352927b
Marcel Vilas <mv@nmc-m.dtag.de>
2014-03-26 10:58:42 +0100

    Exscript: Added new variable 'send_data' allowing to access data send through the protocol if enabled.

--- src/Exscript/protocols/Protocol.py
+++ src/Exscript/protocols/Protocol.py
@@ -261,6 +261,7 @@ class Protocol(object):
         self.response              = None
         self.buffer                = MonitoredBuffer()
         self.account_factory       = account_factory
+        self.send_data             = None
         if stdout is None:
             self.stdout = open(os.devnull, 'w')
         else:
@@ -1130,6 +1131,8 @@ class Protocol(object):
                     termios.tcsetattr(stdin, termios.TCSADRAIN, curtty)
 
                     if not is_handled:
+                        if not self.send_data is None:
+                            self.send_data.write(data)
                         channel.send(data)
         finally:
             termios.tcsetattr(stdin, termios.TCSADRAIN, oldtty)
@@ -1154,6 +1157,8 @@ class Protocol(object):
                 if not data:
                     break
                 if not self._call_key_handlers(key_handlers, data):
+                    if not self.send_data is None:
+                        self.send_data.write(data)
                     channel.send(data)
         except EOFError:
             self._dbg(1, 'User hit ^Z or F6')
