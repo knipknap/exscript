8c838636201967cbf41a98f0ba05c83f33a29640
David Teach <davidc.teach@gmail.com>
2014-03-29 19:29:43 -0700

    Added asa driver.  Edited ph pro curve driver to work with SSH

--- src/Exscript/protocols/drivers/hp_pro_curve.py
+++ src/Exscript/protocols/drivers/hp_pro_curve.py
@@ -18,9 +18,9 @@ A driver for HP ProCurve switches.
 import re
 from Exscript.protocols.drivers.driver import Driver
 
-_user_re       = [re.compile(r'[\r\n]Username: ?$')]
-_password_re   = [re.compile(r'[\r\n]Password: ?$')]
-_prompt_re     = [re.compile(r'[\r\n][\-\w+\.:/]+[>#] ?$')]
+_user_re       = [re.compile(r'[\r\n]?Username: ?$')]
+_password_re   = [re.compile(r'[\r\n]?Password: ?$')]
+_prompt_re     = [re.compile(r'[\r\n]?[\-\w+\.:/]+[>#] ?$')]
 _error_re      = [re.compile(r'(?:invalid|incomplete|ambiguous) input:', re.I)]
 _login_fail_re = [re.compile(r'[\r\n]invalid password', re.I),
                   re.compile(r'unable to verify password', re.I),
