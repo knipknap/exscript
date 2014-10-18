1af983e188879b5b78231821b839162a8dbca9cb
Enno Gröper <groepeen@cms.hu-berlin.de>
2014-10-18 22:06:36 +0200

    Just some unnecessary spelling fixes

--- src/Exscript/protocols/Protocol.py
+++ src/Exscript/protocols/Protocol.py
@@ -52,15 +52,15 @@ class Protocol(object):
     of the API.
 
     The goal of all protocol classes is to provide an interface that
-    is unified accross protocols, such that the adapters may be used
-    interchangably without changing any other code.
+    is unified across protocols, such that the adapters may be used
+    interchangeably without changing any other code.
 
     In order to achieve this, the main challenge are the differences
     arising from the authentication methods that are used.
     The reason is that many devices may support the following variety
-    authentification/authorization methods:
+    authentication/authorization methods:
 
-        1. Protocol level authentification, such as SSH's built-in
+        1. Protocol level authentication, such as SSH's built-in
            authentication.
 
                 - p1: password only
@@ -69,12 +69,12 @@ class Protocol(object):
                 - p4: username + key
                 - p5: username + key + password
 
-        2. App level authentification, such that the authentification may
+        2. App level authentication, such that the authentication may
            happen long after a connection is already accepted.
            This type of authentication is normally used in combination with
            Telnet, but some SSH hosts also do this (users have reported
            devices from Enterasys). These devices may also combine
-           protocol-level authentification with app-level authentification.
+           protocol-level authentication with app-level authentication.
            The following types of app-level authentication exist:
 
                 - a1: password only
@@ -86,7 +86,7 @@ class Protocol(object):
            first serves to authenticate the user, and the second serves to
            authorize him.
            App-level authorization may support the same methods as app-level
-           authentification:
+           authentication:
 
                 - A1: password only
                 - A2: username
@@ -106,7 +106,7 @@ class Protocol(object):
           - a1 - a3: optional
           - A1 - A3: optional
 
-    To achieve authentication method compatibility accross different
+    To achieve authentication method compatibility across different
     protocols, we must hide all this complexity behind one single API
     call, and figure out which ones are supported.
 
@@ -116,14 +116,14 @@ class Protocol(object):
 
         key = PrivateKey.from_file('~/.ssh/id_rsa', 'my_key_password')
 
-        # The user account to use for protocol level authentification.
+        # The user account to use for protocol level authentication.
         # The key defaults to None, in which case key authentication is
         # not attempted.
         account = Account(name     = 'myuser',
                           password = 'mypassword',
                           key      = key)
 
-        # The account to use for app-level authentification.
+        # The account to use for app-level authentication.
         # password2 defaults to password.
         app_account = Account(name      = 'myuser',
                               password  = 'my_app_password',
@@ -140,11 +140,11 @@ class Protocol(object):
     a state such that the following call to expect_prompt() will either
     always work, or always fail.
 
-    We jhide the following methods behind the login() call::
+    We hide the following methods behind the login() call::
 
-        # Protocol level authentification.
+        # Protocol level authentication.
         conn.protocol_authenticate(...)
-        # App-level authentification.
+        # App-level authentication.
         conn.app_authenticate(...)
         # App-level authorization.
         conn.app_authorize(...)
@@ -345,7 +345,7 @@ class Protocol(object):
         The driver argument may be an subclass of protocols.drivers.Driver,
         a known driver name (string), or None.
         If the driver argument is None, the adapter automatically chooses
-        a driver using the the guess_os() function.
+        a driver using the guess_os() function.
 
         @type  driver: Driver|str
         @param driver: The pattern that, when matched, causes an error.
@@ -582,9 +582,9 @@ class Protocol(object):
         argument.
 
         @type  account: Account
-        @param account: The account for protocol level authentification.
+        @param account: The account for protocol level authentication.
         @type  app_account: Account
-        @param app_account: The account for app level authentification.
+        @param app_account: The account for app level authentication.
         @type  flush: bool
         @param flush: Whether to flush the last prompt from the buffer.
         """
@@ -604,9 +604,9 @@ class Protocol(object):
             L{login()}, stick with L{login}.
 
         @type  account: Account
-        @param account: The account for protocol level authentification.
+        @param account: The account for protocol level authentication.
         @type  app_account: Account
-        @param app_account: The account for app level authentification.
+        @param app_account: The account for app level authentication.
         @type  flush: bool
         @param flush: Whether to flush the last prompt from the buffer.
         """
@@ -625,7 +625,7 @@ class Protocol(object):
 
     def protocol_authenticate(self, account = None):
         """
-        Low-level API to perform protocol-level authentification on protocols
+        Low-level API to perform protocol-level authentication on protocols
         that support it.
 
         @note: In most cases, you want to use the login() method instead, as
@@ -1165,7 +1165,7 @@ class Protocol(object):
 
     def guess_os(self):
         """
-        Returns an identifer that specifies the operating system that is
+        Returns an identifier that specifies the operating system that is
         running on the remote host. This OS is obtained by watching the
         response of the remote host, such as any messages retrieved during
         the login procedure.
